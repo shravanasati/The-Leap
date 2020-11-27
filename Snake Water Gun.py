ec3e59f73f8d10de8a3451d97a22576ea23b4dfc
Shravan <69118069+Shravan-1908@users.noreply.github.com>
2020-11-27 19:38:36 +0530

    Update Snake Water Gun.py

--- Snake Water Gun.py
+++ Snake Water Gun.py
@@ -1,111 +1,131 @@
+#snake water gun game
 import random
-import time
+def snake_water_gun():
+    
+    def music(file):
+        from pygame import mixer
+        mixer.init()
+        mixer.music.load(file)
+        mixer.music.play()
 
-outcomes = ["Snake", "Water", "Gun"]
+    win_musiclist = ["anime wow.mp3", "bruhh.mp3"]
+    lose_musiclist = ["Nope.mp3", "Fart.mp3"]
 
-score_comp = 0
-score_user = 0
-ties = 0
-chances = 10
+    def getdate():
+        import datetime
+        return str(datetime.datetime.now())
+    date = getdate()
 
-name = input("What's your name? ")
+    items = ["Snake", "Water", "Gun"]
 
-try:
-    print("WELCOME TO THE SNAKE WATER GUN GAME!!")
-    while chances>0:
-        guess = input("Enter your choice(S/W/G): ")
-        outcomes_choice = random.choice(outcomes)
-        if outcomes_choice == "Snake" and guess in ["g", "G"]:
-            print ("You won this time!")
-            chances = chances - 1
-            print ("Chances left =", chances)
-            score_user += 1
-            time.sleep(1)
-        elif outcomes_choice == "Snake" and guess in ["w", "W"]:
-            print ("You lost this time!")
-            chances = chances - 1
-            print ("Chances left =", chances)
-            score_comp += 1
-            time.sleep(1)
-        elif outcomes_choice == "Snake" and guess in ["s", "S"]:
-            print ("Tie!")
-            print ("Chances left =", chances)
-            chances = chances - 1
-            ties += 1
-            time.sleep(1)
+    score_user = 0
+    score_comp = 0
 
-        elif outcomes_choice == "Water" and guess in ["s", "S"]:
-            print ("You won this time!")
-            chances = chances - 1
-            print ("Chances left =", chances)        
-            score_user += 1
-            time.sleep(1)
-        elif outcomes_choice == "Water" and guess in ["g", "G"]:
-            print ("You lost this time!")
-            chances = chances - 1
-            print ("Chances left =", chances)       
-            score_comp += 1
-            time.sleep(1)
-        elif outcomes_choice == "Water" and guess in ["w", "W"]:
-            print ("Tie!")
-            chances = chances - 1
-            print ("Chances left =", chances)
-            ties += 1
-            time.sleep(1)
+    print ("Welcome to the snake water gun game!\nTen chances would be given and at last score would be announced.")
 
-        elif outcomes_choice == "Gun" and guess in ["w", "W"]:
-            print ("You won this time!")
-            chances = chances - 1
-            print ("Chances left =", chances)
-            score_user += 1
-            time.sleep(1)
-        elif outcomes_choice == "Gun" and guess in ["s", "S"]:
-            print ("You lost this time!")
-            chances = chances - 1
-            print ("Chances left =", chances)
-            score_comp += 1
-            time.sleep(1)
-        elif outcomes_choice == "Gun" and guess in ["g", "G"]:
-            print ("Tie!")
-            chances = chances - 1
-            print ("Chances left =", chances)
-            ties += 1
-            time.sleep(1)
-        else:
-            print ("Invalid input")
-            continue
+    name = input("What's your name? ")
+    print ("You'll have to press S for snake, W for water and G for gun.")
 
-    print ("\t\t\t\t\t*****GAME SUMMARY*****")
-    if score_comp>score_user:
-        print ("\t\t\t\t\tYou lost this game!")
-        print ("\t\t\t\t\tYour score =", score_user)
-        print ("\t\t\t\t\tComputer's score =", score_comp)
-        print ("\t\t\t\t\tTies =", ties)
-        f = open("SWG Game History.txt", 'a')
-        txt_update = (f"{name} lost the game with his score being {score_user} and computer's score being {score_comp} and ties counting to {ties}.\n")
-        f.write(txt_update)
-        f.close()
+    print ("Let's begin now!!")
+    ties = 0
+    chances = 10
 
-    elif score_user>score_comp:
-        print ("\t\t\t\t\tYou won this game!")   
-        print ("\t\t\t\t\tYour score =", score_user)
-        print ("\t\t\t\t\tComputer's score =", score_comp)
-        print ("\t\t\t\t\tTies =", ties)
-        f = open("SWG Game History.txt", 'a')
-        txt_update = (f"{name} won the game with his score being {score_user} and computer's score being {score_comp} and ties counting to {ties}.\n")
-        f.write(txt_update)
-        f.close()
+    try:
+        while chances>0:
+            win_music = random.choice(win_musiclist)
+            lose_music = random.choice(lose_musiclist)
+            item_choice = random.choice(items)
 
-    else:
-        print ("\t\t\t\t\tTie!")
-        print ("\t\t\t\t\tYour score =", score_user)
-        print ("\t\t\t\t\tComputer's score =", score_comp)
-        print ("\t\t\t\t\tTies =", ties)
-        f = open("SWG Game History.txt", 'a')
-        txt_update = (f"{name} had a tie with computer, scores counting {score_user}each and ties being{ties}.\n")
-        f.write(txt_update)
-        f.close()
+            a = input("What's your choice? ")
 
-except Exception as e:
-    print (e)
-    quit()
+            if a in ["s", "S"] and item_choice == "Snake":
+                print ("Tie")
+                music("Awkward Cricket.mp3")
+                ties = ties+1
+                chances = chances - 1
+            elif a in ["s", "S"] and item_choice == "Gun":
+                print ("You lost this time")
+                music(lose_music)
+                score_comp = score_comp +1
+                chances = chances - 1
+            elif a in ["s", "S"] and item_choice == "Water":
+                print ("You won this time")
+                music(win_music)
+                score_user = score_user+1
+                chances = chances - 1
+
+            elif a in ["w", "W"] and item_choice == "Snake":
+                print ("You lost this time")
+                music(lose_music)
+                score_comp += 1
+                chances = chances - 1
+            elif a in ["w", "W"] and item_choice == "Gun":
+                print ("You won this time")
+                music(win_music)
+                score_user += 1
+                chances = chances - 1
+            elif a in ["w", "W"] and item_choice == "Water":
+                print ("Tie")
+                music("Awkward Cricket.mp3")
+                ties = ties+1
+                chances = chances - 1
+
+            elif a in ["g", "G"] and item_choice == "Snake":
+                print ("You won this time")
+                music(win_music)
+                score_user += 1
+                chances = chances - 1
+            elif a in ["g", "G"] and item_choice == "Gun":
+                print ("Tie")
+                music("Awkward Cricket.mp3")
+                ties = ties+1
+                chances = chances - 1
+            elif a in ["g", "G"] and item_choice == "Water":
+                print ("You lost this time")
+                music(lose_music)
+                score_comp = score_comp+1
+                chances = chances - 1
+
+            else:
+                print ("Invalid input!")
+                continue
+
+            if chances == 0:
+
+                music("piano result.mp3")
+                print ("Game over! Let's take look at the scores:")
+
+                print ("Your score=", score_user)
+
+                print ("Computer's score=", score_comp)
+
+                print ("Ties=", ties)
+
+
+                if score_user>score_comp:
+                    print ("You won this game", name.capitalize()+"!!!")
+                    f = open("SWG game history.txt", 'a')
+                    txt_update = f"On {date}, {name.capitalize()} won the game with score= {score_user}, beating computer who had score= {score_comp} with {ties} ties.\n"
+                    f.write(txt_update)
+                    f.close
+
+                elif score_user<score_comp:
+                    print ("You lost this game", name.capitalize()+"!")
+                    f = open("SWG game history.txt", 'a')
+                    txt_update = f"On {date}, {name.capitalize()} lost the game with score= {score_user}, beaten by computer who had score= {score_comp} with {ties} ties.\n"
+                    f.write(txt_update)
+                    f.close
+
+                else:
+                    print (f"{name.capitalize()}, you have a tie with computer!")
+                    f = open("SWG game history.txt", 'a')
+                    txt_update = f"On {date}, {name.capitalize()} had a tie with computer, total number of ties counting to {ties} and respective score being {score_user} and {score_comp}.\n"
+                    f.write(txt_update)
+                    f.close
+
+    except Exception as e:
+        print (e)
+        quit()
+
+if __name__ == "__main__":
+    snake_water_gun()
